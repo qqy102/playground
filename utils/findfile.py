7eb2fbf785546d57c557a85b5634b708353251e1
qqy102 <quentiny@gmail.com>
2018-06-27 21:57:49 +0800

    add args, identify audio file types

--- utils/findfile.py
+++ utils/findfile.py
@@ -1,15 +1,49 @@
 #!/usr/bin/env python
 
 import os
+import sys
+import argparse
+
+CDImageName = 'CDImage'
+cue = '.cue'
+ape = '.ape'
+flac = '.flac'
+wav = '.wav'
+audioExtList = [ape, cue, flac, wav]
+
+jpg = '.jpg'
+png = '.png'
+picExtList = [jpg, png]
+
 
 def walkdir(dir):
     for dirpath, dirs, files in os.walk(dir):
+        CDImageDic = {}
         for filename in files:
+            basename, ext = os.path.splitext(filename)
+            if ext in audioExtList:
+                if CDImageName in basename:
+                    if basename not in CDImageDic: 
+                        CDImageDic[basename] = []
+                    CDImageDic[basename].append(ext)
+                    #print basename, ext
             fname = os.path.join(dirpath, filename)
-            print fname
+        print CDImageDic    
+
             #with open(fname) as myfile:
             #    print(myfile.read())
 
+def createArgParser():
+    parser = argparse.ArgumentParser(description='QQ music file info grabber')
+    parser.add_argument('inputDir', help='input Dir')
+    return parser
+
 if __name__ == '__main__':
     dir = "."
-    walkdir(dir)
+    argParser = createArgParser()
+    parsedArgs = argParser.parse_args(sys.argv[1:])
+    if os.path.exists(parsedArgs.inputDir):
+        dir = parsedArgs.inputDir
+        walkdir(dir)
+    else:
+        print 'No Input Dir Assigned'
